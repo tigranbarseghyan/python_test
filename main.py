21a96b87d3420ae3e1c031a0a35da6006d83f289
tigranbarseghyan <tigran.barsegyan@gmail.com>
2024-12-05 11:39:05 +0400

    Added first tests and github action

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def msum(a, b):
+    return a + b
+
+def msub(a, b):
+    return a - b
+
---
+++ my_test.py
@@ -0,0 +1,7 @@
+import main
+
+def test_sum():
+    assert main.msum(10, 20) == 30
+
+def test_sub():
+    assert main.msub(20, 10) == 10
